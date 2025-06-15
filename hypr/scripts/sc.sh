48f3f0e5d1b92f0b1fc145ff7f1f22aca2206b73
takebreak1 <aaaryanbajaj02@gmail.com>
2025-06-15 20:04:37 +0530

    Remove unknown folder

--- hypr/scripts/sc.sh
+++
@@ -1,8 +0,0 @@
-region="$(slurp)"
-
-if [ -z "$region" ]; then
-    exit 1
-fi
-filename="$HOME/Pictures/Screenshot/Screenshot-$(date +'%d-%m-%Y_%H-%M-%S').png"
-grim -g "$region" "$filename"
-notify-send "Screenshot captured" "$filename"
