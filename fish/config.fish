b4fe9b6998dc4fa25229d7f46f212e891ee6102b
takebreak1 <aaaryanbajaj02@gmail.com>
2025-07-06 22:26:41 +0530

    Prefinal

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    alias hc="nvim ~/.config/hypr/hyprland.conf"
+    alias nv="nvim"
+    alias cn="cd ~/.config"
+    alias up="sudo dnf update"
+    alias in="sudo dnf install"
+    alias nvc="nvim ~/.config/nvim"
+    fish_config theme choose Nord
+    set fish_color_error red --bold
+    starship init fish | source
+end
